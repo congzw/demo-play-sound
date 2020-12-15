780545cb0d64587ff0db57207853afb040e6329d
congzw <2981810300@qq.com>
2020-12-15 10:46:03 +0800

    add web invoke demo

---
+++ src/NbSites.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NbSites.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Play([FromServices] IHostingEnvironment env)
+        {
+            var filePath = Path.Combine(env.WebRootPath, "content", "sounds", "Alarm002.wav");
+            if (!System.IO.File.Exists(filePath))
+            {
+                ViewBag.Message = "FileNotExist:" + filePath;
+            }
+            else
+            {
+                ViewBag.Message = "PlaySound:" + filePath;
+                PlaySound(filePath);
+            }
+            return View("Index");
+        }
+
+        private static void PlaySound(string file)
+        {
+            Process.Start(@"powershell", $@"-c (New-Object Media.SoundPlayer '{file}').PlaySync();");
+        }
+    }
+}
---
+++ src/NbSites.Web/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NbSites.Web.Boots;
+
+namespace NbSites.Web
+{
+    public class Startup
+    {
+        private readonly ILogger<Startup> _logger;
+        private readonly IHostingEnvironment _env;
+
+        public Startup(ILogger<Startup> logger, IHostingEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (_env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMyStaticFiles(_env, _logger);
+            
+            //do not forget to add[Area("Foo")] in controllers of area
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "route_root",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
