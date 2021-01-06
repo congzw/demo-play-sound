1f5949906caeda311e319513071042da29ddf02f
congzw <2981810300@qq.com>
2021-01-06 19:02:30 +0800

    修改文字

--- src/NbSites.Web3x/Libs/PlaySounds/PlaySoundFileRepository.cs
+++ src/NbSites.Web3x/Libs/PlaySounds/PlaySoundFileRepository.cs
@@ -8,9 +8,9 @@ namespace NbSites.Web.Libs.PlaySounds
         public PlaySoundFileRepository()
         {
             //content", "sounds", "Alarm002.wav"
-            PlaySoundFiles.Add(new PlaySoundFile() { Id = "Default", FilePath = "~/content/sounds/default.wav", Description = "默认" });
-            PlaySoundFiles.Add(new PlaySoundFile() { Id = "Alarm001", FilePath = "~/content/sounds/alarm001.wav", Description = "犬吠" });
-            PlaySoundFiles.Add(new PlaySoundFile() { Id = "Alarm002", FilePath = "~/content/sounds/alarm002.wav", Description = "打雷" });
+            PlaySoundFiles.Add(new PlaySoundFile() { Id = "Default", FilePath = "~/content/sounds/default.wav", Description = "打雷" });
+            PlaySoundFiles.Add(new PlaySoundFile() { Id = "Alarm001", FilePath = "~/content/sounds/alarm001.wav", Description = "警报1" });
+            PlaySoundFiles.Add(new PlaySoundFile() { Id = "Alarm002", FilePath = "~/content/sounds/alarm002.wav", Description = "警报2" });
             PlaySoundFiles.Add(new PlaySoundFile() { Id = "fail", FilePath = "~/content/sounds/fail.wav", Description = "构建失败" });
             PlaySoundFiles.Add(new PlaySoundFile() { Id = "buildComplete", FilePath = "~/content/sounds/buildComplete.wav", Description = "构建完成" });
             PlaySoundFiles.Add(new PlaySoundFile() { Id = "deployComplete", FilePath = "~/content/sounds/deployComplete.wav", Description = "部署完成" });
